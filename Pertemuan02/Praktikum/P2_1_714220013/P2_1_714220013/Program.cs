2c92d5ba76b737b96e6c691c094e586a6f0ff107
barganakukuhraditya <barganakukuh@gmail.com>
2023-10-04 15:32:57 +0700

    Praktikum1

---
+++ Pertemuan02/Praktikum/P2_1_714220013/P2_1_714220013/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714220013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arithmetic Operators
+            Console.WriteLine("Masukan Angka Pertama :");
+            int angka1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Masukan Angka Kedua :");
+            int angka2 = Convert.ToInt32 (Console.ReadLine());
+
+            int penjumlahan = angka1 + angka2;
+            Console.WriteLine("{0} + {1} = {2}", angka1, angka2, penjumlahan);
+
+            int pengurangan = angka1 - angka2;
+            Console.WriteLine("{0} - {1} = {2}", angka1, angka2, pengurangan);
+
+            int perkalian = angka1 * angka2;
+            Console.WriteLine("{0} * {1} = {2}", angka1, angka2, perkalian);
+
+           double pembagian = (double)angka1 / angka2;
+            Console.WriteLine("{0} / {1} = {2}", angka1, angka2, pembagian);
+
+
+        }
+    }
+}
