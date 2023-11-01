504c20cd37cd747ee9d6564e30e5ff6dbcb2384f
barganakukuhraditya <barganakukuh@gmail.com>
2023-11-01 16:12:01 +0700

    Pertemuan6

---
+++ Pertemuan06/Praktikum/P6_3_714220013/P6_3_714220013/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace P6_3_714220013
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtHuruf_Leave(object sender, EventArgs e)
+        {
+            if (txtHuruf.Text == "")
+            {
+                epWarning.SetError(txtHuruf, "Textbox Huruf tidak boleh kosong!");
+                epWrong.SetError(txtHuruf, "");
+                epCorrect.SetError(txtHuruf, "");
+            }
+            else
+            {
+                if ((txtHuruf.Text).All(Char.IsLetter))
+                {
+                    epWarning.SetError(txtHuruf, "");
+                    epWrong.SetError(txtHuruf, "");
+                    epCorrect.SetError(txtHuruf, "Betul!");
+                }
+                else
+                {
+                    epWarning.SetError(txtHuruf, "Inputan hanya boleh huruf!");
+                    epWrong.SetError(txtHuruf, "");
+                    epCorrect.SetError(txtHuruf, "");
+                }
+            }
+        }
+
+        private void txtAngka_Leave(object sender, EventArgs e)
+        {
+            if (txtAngka.Text == "")
+            {
+                epWarning.SetError(txtAngka, "Textbox Angka tidak boleh kosong!");
+                epWrong.SetError(txtAngka, "");
+                epCorrect.SetError(txtAngka, "");
+            }
+            else
+            {
+                if ((txtAngka.Text).All(Char.IsNumber))
+                {
+                    epCorrect.SetError(txtAngka, "Betul!");
+                    epWarning.SetError(txtAngka, "");
+                    epWrong.SetError(txtAngka, "");
+                }
+                else
+                {
+                    epCorrect.SetError(txtAngka, "");
+                    epWarning.SetError(txtAngka, "");
+                    epWrong.SetError(txtAngka, "Inputan hanya boleh angka!");
+                }
+            }
+        }
+
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(txtEmail.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                epWarning.SetError(txtEmail, "");
+                epWrong.SetError(txtEmail, "");
+                epCorrect.SetError(txtEmail, "Betul!");
+            }
+            else
+            {
+                epWarning.SetError(txtEmail, "");
+                epWrong.SetError(txtEmail, "Format email salah!\nContoh: a@b.c");
+                epCorrect.SetError(txtEmail, "");
+            }
+        }
+
+        private void txtAngka1_Leave(object sender, EventArgs e)
+        {
+            if (IsNumber(txtAngka1.Text) && IsNumber(txtAngka2.Text))
+            {
+                int angka1 = Convert.ToInt32(txtAngka1.Text);
+                int angka2 = Convert.ToInt32(txtAngka2.Text);
+
+                if (angka1 <= angka2)
+                {
+                    epWarning.SetError(txtAngka2, "Angka1 harus lebih besar dari Angka2");
+                    epWrong.SetError(txtAngka2, "");
+                    epCorrect.SetError(txtAngka2, "");
+                }
+                else
+                {
+                    epWarning.SetError(txtAngka2, "");
+                    epWrong.SetError(txtAngka2, "");
+                    epCorrect.SetError(txtAngka2, "Betul");
+                }
+            }
+            else
+            {
+                epWarning.SetError(txtAngka2, "Masukkan angka yang valid");
+                epWrong.SetError(txtAngka2, "");
+                epCorrect.SetError(txtAngka2, "");
+            }
+
+        }
+        private bool IsNumber(string text)
+        {
+            int number;
+            return int.TryParse(text, out number);
+        }
+
+        private void txtAngka2_Leave(object sender, EventArgs e)
+        {
+            if (IsNumber(txtAngka1.Text) && IsNumber(txtAngka2.Text))
+            {
+                int angka1 = Convert.ToInt32(txtAngka1.Text);
+                int angka2 = Convert.ToInt32(txtAngka2.Text);
+
+                if (angka1 <= angka2)
+                {
+                    epWarning.SetError(txtAngka1, "Angka1 harus lebih besar dari Angka2");
+                    epWrong.SetError(txtAngka1, "");
+                    epCorrect.SetError(txtAngka1, "");
+                }
+                else
+                {
+                    epWarning.SetError(txtAngka1, "");
+                    epWrong.SetError(txtAngka1, "");
+                    epCorrect.SetError(txtAngka1, "Betul");
+                }
+            }
+            else
+            {
+                epWarning.SetError(txtAngka1, "Masukkan angka yang valid");
+                epWrong.SetError(txtAngka1, "");
+                epCorrect.SetError(txtAngka1, "");
+            }
+
+        }
+
+    }
+    
+}
